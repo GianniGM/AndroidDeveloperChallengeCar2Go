baca53c7290184e5c37b6ff0309110b49808c82b
Gianni G <giangraziano.modica@gruppometa.it>
2018-04-09 17:53:24 +0200

    added a tag

--- app/src/main/java/giangraziano/it/androiddeveloperchallengecar2go/MainActivity.kt
+++ app/src/main/java/giangraziano/it/androiddeveloperchallengecar2go/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MAIN_ACTIVITY"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
