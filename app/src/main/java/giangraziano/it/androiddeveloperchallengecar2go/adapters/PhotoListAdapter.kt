cbd13121c6fdec955020481bbce1c49f0b92943d
Gianni G <giangraziano.modica@gruppometa.it>
2018-04-11 16:55:17 +0200

    Changed recyclerView version to 27 and applied relative changes on PhotoListAdapter

--- app/src/main/java/giangraziano/it/androiddeveloperchallengecar2go/adapters/PhotoListAdapter.kt
+++ app/src/main/java/giangraziano/it/androiddeveloperchallengecar2go/adapters/PhotoListAdapter.kt
@@ -15,6 +15,7 @@ import giangraziano.it.androiddeveloperchallengecar2go.model.Photo
 const val PLACEHOLDER_PICTURE = "http://www.pixedelic.com/themes/geode/demo/wp-content/uploads/sites/4/2014/04/placeholder4.png";
 
 class PhotoListAdapter : RecyclerView.Adapter<PhotoListAdapter.PhotoViewHolder>() {
+
     private var photos: MutableList<Photo> = mutableListOf()
 
     fun setData(list: MutableList<Photo>){
@@ -22,9 +23,9 @@ class PhotoListAdapter : RecyclerView.Adapter<PhotoListAdapter.PhotoViewHolder>(
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PhotoViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         //build my itemView
-        val ctx = parent?.context
+        val ctx = parent.context
         val inflate = LayoutInflater.from(ctx)
         val v = inflate.inflate(R.layout.image_list_item, parent, false)
         return PhotoViewHolder(v)
@@ -34,17 +35,17 @@ class PhotoListAdapter : RecyclerView.Adapter<PhotoListAdapter.PhotoViewHolder>(
         return photos.size
     }
 
-    override fun onBindViewHolder(holder: PhotoViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
         val item = photos[position]
         val url: String = item.urls.thumb ?: PLACEHOLDER_PICTURE
 
-        holder?.setImage(url, 200 , 400)
+        holder.setImage(url)
     }
 
-    inner class PhotoViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    inner class PhotoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val imageView = view.findViewById(R.id.item_image_item) as ImageView
 
-        fun setImage(imageUrl: String, width: Int, height: Int) {
+        fun setImage(imageUrl: String) {
 
             Picasso
                     .get()
