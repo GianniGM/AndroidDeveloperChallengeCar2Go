28f774f5b10bb237c9f484a7c2e45953f3872d40
GianniGM <giangraziano@gmail.com>
2018-04-13 00:00:38 +0200

    added right issues

--- app/src/main/java/it/giangraziano/androiddeveloperchallengecar2go/network/UnsplashService.kt
+++ app/src/main/java/it/giangraziano/androiddeveloperchallengecar2go/network/UnsplashService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface UnsplashService {
-    @GET("photos/")
+    @GET("/photos/")
     fun getPhotos(
             @Query("client_id") clientId: String,
             @Query("page") page: Int
