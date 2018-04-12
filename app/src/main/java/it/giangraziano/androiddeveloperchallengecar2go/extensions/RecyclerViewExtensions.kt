95a6787bd06bcdad6b2bd26676bb6102ff34e856
GianniGM <giangraziano@gmail.com>
2018-04-12 23:59:39 +0200

    changed to StaggeredLayout

--- app/src/main/java/it/giangraziano/androiddeveloperchallengecar2go/extensions/RecyclerViewExtensions.kt
+++ app/src/main/java/it/giangraziano/androiddeveloperchallengecar2go/extensions/RecyclerViewExtensions.kt
@@ -1,8 +1,8 @@
 package it.giangraziano.androiddeveloperchallengecar2go.extensions
 
 import android.content.Context
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 
 
 fun RecyclerView.setColumnsLayout(ctx: Context) {
@@ -10,15 +10,15 @@ fun RecyclerView.setColumnsLayout(ctx: Context) {
     val dpWidth = displayMetrics.widthPixels / displayMetrics.density
     val nColumns = (dpWidth / 180).toInt()
 
-    layoutManager = GridLayoutManager(ctx, nColumns)
+    layoutManager = StaggeredGridLayoutManager(nColumns, StaggeredGridLayoutManager.VERTICAL)
 }
 
 fun RecyclerView.onScrollToEnd(whenScrollCloseToEnd: (Unit) -> Unit) =
         addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
 
-                val manager = layoutManager as GridLayoutManager
-                if (manager.childCount + manager.findFirstVisibleItemPosition()
+                val manager = layoutManager as StaggeredGridLayoutManager
+                if (manager.childCount + manager.findFirstVisibleItemPositions(null)[0]
                         >= layoutManager.itemCount - 10) {
                     whenScrollCloseToEnd(kotlin.Unit)
                 }
